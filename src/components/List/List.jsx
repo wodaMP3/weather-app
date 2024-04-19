feaf0a07d5e99e90e06b40e2fd894d67d7f64d08
Artem <bezkorovainy2006@gmail.com>
2024-04-19 20:40:44 +0200

    updated css for list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,6 @@
 import './List.css';
 import { Card } from 'semantic-ui-react';
+import moment from 'moment';
 
 const List = ({ weatherData }) => {
     return (
@@ -10,6 +11,8 @@ const List = ({ weatherData }) => {
                     <p className='temp'>Temperature: {weatherData.main.temp}&deg;C</p>
                     <p className='desc'>Weather: {weatherData.weather[0].main}</p>
                     <p>Humidity: {weatherData.main.humidity} %</p>
+                    <div className='flex'><p>Day: {moment().format('dddd')}</p></div>
+                    <p>Date: {moment().format('LL')}</p>
                 </Card.Content>
             )}
         </Card>
