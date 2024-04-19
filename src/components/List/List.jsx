e858f7a4011ed2d594eca3b09801bf9f4805d72d
Artem <bezkorovainy2006@gmail.com>
2024-04-20 00:13:06 +0200

    added textColor func in list.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,12 +3,27 @@ import { Card } from 'semantic-ui-react';
 import moment from 'moment';
 
 const List = ({ weatherData }) => {
+    
+    const textColor = (num) =>  {
+        let color
+        switch (true) {
+            case num > 2:
+                color = 'red';
+                break;
+            case num <= 0:
+                color = 'blue';
+                break;
+                default:
+                    color = 'black';
+            }
+            return color;
+    }
     return (
         <Card className='list'>
             {weatherData && (
                 <Card.Content>
                     <Card.Header className='header'>{weatherData.name}</Card.Header>
-                    <p className='temp'>Temperature: {weatherData.main.temp}&deg;C</p>
+                    <p className='temp' style={{ color: textColor(weatherData.main.temp)}}>Temperature: {weatherData.main.temp}&deg;C</p>
                     <p className='desc'>Weather: {weatherData.weather[0].main}</p>
                     <p>Humidity: {weatherData.main.humidity} %</p>
                     <div className='flex'><p>Day: {moment().format('dddd')}</p></div>
