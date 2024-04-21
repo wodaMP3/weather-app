8368348ad41f4e8b050e412853d45dd7e7504152
Artem <bezkorovainy2006@gmail.com>
2024-04-21 20:08:36 +0200

    added ui autocomplete

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,45 +1,53 @@
 import { useState } from "react";
+import axios from "axios";
 import './Form.css';
 
+const apiKey = import.meta.env.VITE_API_KEY;
+const API_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-const Form = ({ onSubmit, ...props }) => {
+const Form = ({ onSubmit }) => {
     const [city, setCity] = useState('');
     const [suggestions, setSuggestions] = useState([]);
-    const options = ['apple'];
 
-    const handleCityChange = (e) => {
-        setCity(e.target.value);
+    const handleCityChange = async (e) => {
+        const value = e.target.value;
+        setCity(value);
+
+        if (value.trim() === ''){
+            setSuggestions([]);
+            return;
+        }
+
+        try {
+            const response = await axios.get(`${API_BASE_URL}?q=${value}&appid=${apiKey}`);
+            const cityName = response.data.name;
+            setSuggestions([cityName]);
+        } catch (error) {
+            console.error('Failed to fetch city data:', error);
+            setSuggestions([]);
+        }
     };
 
-   
     const handleSubmit = (e) => {
         e.preventDefault();
         onSubmit(city);
     };
 
     return (
-        <form className="form"
-            onSubmit={handleSubmit}>    
+        <form className="form" onSubmit={handleSubmit}>    
             <input
                 className="input"
                 type="text"
                 value={city}
                 placeholder="Enter the city name"
-        onChange={handleCityChange}/>
-        <ul className="autocomplete">
-            <li className="autocomplete__item">sdd</li>
-            <li className="autocomplete__item">asd</li>
-            <li className="autocomplete__item">asd</li>
-            <li className="autocomplete__item">asd</li>
-            <li className="autocomplete__item">asd</li>
-            <li className="autocomplete__item">asd</li>
-            <li className="autocomplete__item">asd</li>
-            <li className="autocomplete__item">asd</li>
-            <li className="autocomplete__item">ad</li>
-            <li className="autocomplete__item">asd</li>
-        </ul>
-       
-            <button className="buttonSearch">get weather</button>
+                onChange={handleCityChange}
+            />
+            <ul className="autocomplete">
+                {suggestions.map((city, index) => (
+                    <li className='autocomplete__item' key={index}>{city}</li>
+                ))}
+            </ul>
+            <button className="buttonSearch">Get weather</button>
         </form>
     );
 };
